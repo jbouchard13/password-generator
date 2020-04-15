6d04560f8ab22e5f00fb8611c4e478d84e8bc14d
John Bouchard <jbouchard13@gmail.com>
2020-04-14 22:58:45 -0400

    removed log commands and added arrays with characters

--- Develop/script.js
+++ Develop/script.js
@@ -12,3 +12,18 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+// Arrays for password criteria
+
+//Lowercase Array
+var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+//Uppercase Array
+var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+//Number Array
+var numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+//Special Character Array
+var specChars = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "'", ";", "", "<", ">", ",", ".", "/"]
